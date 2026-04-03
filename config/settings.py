a5ceeddb9d04f3e9273b4a106612777f11ba5eba
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-04-03 11:00:13 +0200

    add JWT authentication and update API routes

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "content",
     "users",
 ]
@@ -70,6 +71,12 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
 
 WSGI_APPLICATION = "config.wsgi.application"
 
--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("content.urls")),
+    path("api/content", include("content.urls")),
+    path("api/auth/", include("users.urls")),
 ]
 
 app_name = "config"
